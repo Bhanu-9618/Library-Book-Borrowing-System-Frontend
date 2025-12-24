cc807ff5f49162938f5e29d495a7ae1c48fd83e1
nipunbhanuka12@gmail.com <nipunbhanuka12@gmail.com>
2025-12-24 17:34:46 +0530

    Create service package for set apis and add book controller APIs

---
+++ src/service/api.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+const BASE_URL = "http://localhost:8080";
+
+export const getAllBooks = () => axios.get(`${BASE_URL}/book/all`);
+export const saveBook = (book) => axios.post(`${BASE_URL}/book/save`, book);
+export const updateBook = (book) => axios.put(`${BASE_URL}/book/update`, book);
+export const deleteBook = (id) => axios.delete(`${BASE_URL}/book/delete/${id}`);
+export const getBookById = (id) => axios.get(`${BASE_URL}/book/id/${id}`);
